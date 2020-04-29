c8cf956f46054ed81f3a60e35f0442079cbb7ede
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-04-29 14:09:40 +0100

    Remove warning from build

--- gh-page/src/components/shared/TopBar.js
+++ gh-page/src/components/shared/TopBar.js
@@ -1,12 +1,9 @@
 import React from "react";
-import TopBarNavigation from "./TopBarNavigation";
 import logo from "../../img/logo.svg";
 
-var TopBar = ({ onApiKeyChange, apiKey, usePublicKey, togglePublicKey }) => {
+var TopBar = () => {
     return (
         <div className="app-header">
-            {/* <TopBarNavigation />
-            <div className="logo" tabIndex="1" ></div> */}
             <div className="logo"><img src={logo} alt="Logo" height="90" /></div>
         </div>
     );
