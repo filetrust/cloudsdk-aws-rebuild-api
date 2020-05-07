aa05f3a4a7dd2880e4ac843115bc45d5e7521b79
Glasswall-Github <64907318+Glasswall-Github@users.noreply.github.com>
2020-05-07 11:00:06 +0000

    Deploying to gh-pages from  @ a7be62e576d66b915b0b9f2cff8b01fb12c860a0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cloudsdk-aws-rebuild-api/precache-manifest.6804b6527646b72df3ec26371376faa3.js"
+  "/cloudsdk-aws-rebuild-api/precache-manifest.86c8e061074bf9bfc73b817d12438df6.js"
 );
 
 self.addEventListener('message', (event) => {
