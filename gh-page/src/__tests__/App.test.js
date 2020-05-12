e181fdbab7ba1421cfbfcfd5ffb0f1b1312d2c77
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-05-12 13:45:04 +0100

    Add handful of unit and integration tests, also fix some css issues

--- gh-page/src/__tests__/App.test.js
+++ gh-page/src/__tests__/App.test.js
@@ -1,7 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import {
+    HashRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
+
 import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import App from "../components/App";
+import App from "../components/App.js";
+import SwaggerPage from "../components/swagger/SwaggerPage.js";
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -14,5 +22,25 @@ describe("App", () => {
   it("matches snapshot", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+  
+  it("renders router", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<Router/>)).toBeDefined();
+  });
+
+  it("renders route", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<Route/>)).toBeDefined();
+  });
+  
+  it("renders switch", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<Switch />)).toBeDefined();
+  });
+
+  it("renders swaggerpage", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<SwaggerPage />)).toBeDefined();
   });
 });
