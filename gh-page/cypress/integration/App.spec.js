4d5a655a5b8c3a79669269c098fa7dd82d8f2cb8
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-05-12 13:45:04 +0100

    Add handful of unit and integration tests, also fix some css issues

---
+++ gh-page/cypress/integration/App.spec.js
@@ -0,0 +1,84 @@
+
+/* eslint-disable no-undef */
+
+const apiKey = Cypress.env("x-api-key");
+
+describe("Swagger Page", () => {
+    describe("layout", () => {
+        beforeEach(() => {
+            cy.visit("/#/");
+        });
+
+        it("title is correct", () => {
+            cy.get(".title").should("have.text", "Glasswall File Rebuild Product API 0.1.9 OAS3");
+        });
+    
+        it("Renders each endpoint", () => {
+            cy.get(".opblock").should("have.lengthOf", "3")
+        });
+        
+        it("Renders correct server", () => {
+            cy.get(".servers option").should("have.value", "https://8oiyjy8w63.execute-api.us-west-2.amazonaws.com/Prod")
+        });
+    })
+    
+    describe("url to url endpoint", () => {
+        beforeEach(() => {
+            cy.visit("/#/");
+            cy.get(".opblock").eq(0).click();
+            cy.get(".opblock").eq(0).find(".try-out__btn").click();
+        });
+
+        it("Request details are correct should be post", () => {
+            cy.get(".opblock").eq(0).find(".opblock-summary-method").should("have.text", "POST")
+            cy.get(".opblock").eq(0).find(".parameter__name.required").contains("x-api-key");
+            cy.get(".opblock").eq(0).find(".opblock-section .content-type").contains("application/json");
+        });
+        
+        it("Should be able to submit a valid form", () => {
+            cy.get(".opblock").eq(0).find("input[placeholder=x-api-key]").type(apiKey);
+            cy.get(".opblock").eq(0).find("button.execute").click();
+            cy.get(".opblock").eq(0).contains("Server response");
+        });
+    });
+    
+    describe("file endpoint", () => {
+        beforeEach(() => {
+            cy.visit("/#/");
+            cy.get(".opblock").eq(1).click();
+            cy.get(".opblock").eq(1).find(".try-out__btn").click();
+        });
+
+        it("Request details are correct should be post", () => {
+            cy.get(".opblock").eq(1).find(".opblock-summary-method").should("have.text", "POST")
+            cy.get(".opblock").eq(1).find(".parameter__name.required").contains("x-api-key");
+            cy.get(".opblock").eq(1).find(".opblock-section .content-type").contains("multipart/form-data");
+        });
+        
+        it("Should be able to submit a valid form", () => {
+            cy.get(".opblock").eq(1).find("input[placeholder=x-api-key]").type(apiKey);
+            cy.get(".opblock").eq(1).find("button.execute").click();
+            cy.get(".opblock").eq(1).contains("Server response");
+        });
+    });
+        
+    describe("base64 endpoint", () => {
+        before(() => {
+            cy.visit("/#/");
+            cy.get(".opblock").eq(2).click();
+            cy.get(".opblock").eq(2).find(".try-out__btn").click();
+        });
+
+        it("Request details are correct should be post", () => {
+            cy.get(".opblock").eq(2).find(".opblock-summary-method").should("have.text", "POST")
+            cy.get(".opblock").eq(2).find(".parameter__name.required").contains("x-api-key");
+            cy.get(".opblock").eq(2).find(".opblock-section .content-type").contains("application/json");
+        });
+        
+        it("Should be able to submit a valid form", () => {
+            cy.get(".opblock").eq(2).find("input[placeholder=x-api-key]").type(apiKey);
+            cy.get(".opblock").eq(2).find("button.execute").click();
+            cy.get(".opblock").eq(2).contains("Server response");
+        });
+    });
+});
--- gh-page/src/components/swagger/SwaggerPage.js
+++ gh-page/src/components/swagger/SwaggerPage.js
@@ -3,6 +3,7 @@ import yam from "./api.yaml";
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 import TopBar from "../shared/TopBar.js";
+import Base64Injector from "./Base64Injector.js";
 
 var SwaggerPage = () => {
     return (
@@ -10,54 +11,9 @@ var SwaggerPage = () => {
             <TopBar />
             <SwaggerUI url={window.location.origin + yam}
                 docExpansion="list"
-                defaultModelExpandDepth="1"
-                requestInterceptor={request => _loadBase64IntoRequestIfSpecified(request)} />
+                defaultModelExpandDepth={1}
+                requestInterceptor={Base64Injector.TryInjectSelectedFileAsBase64} />
         </>);
-}
-
-async function _loadBase64IntoRequestIfSpecified(request) {
-    if (!request.url.toLowerCase().includes("base64"))
-    {
-        return request;
-    }
-
-    const input = document.getElementById("base64Input");
-
-    if (!input || !input.value || input.value === "") {
-        return request;
-    }
-
-    const fileBuffer = await _readInputFileBuffer(input.files[0])
-    const fileBase64 = _arrayBufferToBase64(fileBuffer);
-
-    var bodyParsed = JSON.parse(request.body);
-    bodyParsed.Base64 = fileBase64;
-
-    request.body = JSON.stringify(bodyParsed);
-    return request;
-};
-
-async function _readInputFileBuffer(file) {
-    return await new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsArrayBuffer(file);
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.onerror = error => {
-            reject(error);
-        }
-    });
-}
-
-function _arrayBufferToBase64(buffer) {
-    var binary = '';
-    var bytes = new Uint8Array(buffer);
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
 }
 
 export default SwaggerPage;
