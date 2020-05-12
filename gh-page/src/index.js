cc73e3bdceeebeef8cbfcc01d5e6f9fa199eea3c
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-05-12 13:45:04 +0100

    Add handful of unit and integration tests, also fix some css issues

--- gh-page/src/index.js
+++ gh-page/src/index.js
@@ -2,22 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import "./App.css";
-import SwaggerPage from "./components/swagger/SwaggerPage";
+import App from "./components/App.js";
 
-import {
-    HashRouter as Router,
-    Route,
-    Switch
-} from "react-router-dom";
-
-var Root = () => {
-  return (
-  <Router basename='/'>
-    <Switch>
-      <Route><SwaggerPage /></Route>
-    </Switch>
-  </Router>);
-}
-
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
